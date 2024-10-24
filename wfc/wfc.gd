2a35d5aa301173ac2289afd6f146f1c5379173a4
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-23 22:22:41 -0700

    Refactor wfc.gd
    - Changed global variable names to be more descriptive
    - Added ; to missing lines
    - enforced snake case naming
    - Removed debug print statements
    - Changed some of the function variable names to be more descriptive

--- wfc/wfc.gd
+++ wfc/wfc.gd
@@ -6,106 +6,109 @@ func _ready() -> void:
 	model.model_generated.connect(pre_initialize);
 	model.setup();
 
-
 func generate_mapping() -> bool:
 	return true;
 
 # not defined?
-var weights = []
+var weights: Array[float] = [];
 
+var image_width_tiles: int = 0;
+var image_height_tiles: int = 0;
+var total_tile_count: int = 0;
+var total_pattern_count: int = 0;
 
-var fmx = 0
-var fmy = 0
-var fmx_x_fmy = 0
-var t = 0;
-var n = 0;
-var initialized_field = false;
-var generation_complete = false;
+var pattern_size: int = 0; #Assuming square patterns only in this implementation
 
-var wave = null;
-var compatible = null;
-var weight_log_weights = null;
-var sum_of_weights = 0;
-var sum_of_weight_log_weights = 0;
+var initialized_field: bool = false;
+var generation_complete: bool = false;
 
-var starting_entropy = 0;
+var wave = null; #Actually Array[Array[bool]] (cant check, blame GDScript)
+var compatibility_matrix = null; #Actually Array[Array[Array[int]]] (cant check, blame GDScript)
+var log_weighted_sums: Array[float] = [];
+var total_log_weighted_sums: int = 0;
+var total_weights: int = 0;
 
-var sums_of_ones = null;
-var sums_of_weights = null;
-var sums_of_weights_log_weights = null;
-var entropies = null;
+var initial_entropy: int = 0;
 
-var observed = null;
-var distribution = null;
+# Arrays that keep track of data for each tile
+var tile_possible_pattern_count: Array[int] = [];
+var tile_total_pattern_weights: Array[float] = [];
+var tile_pattern_log_weight_sums: Array[float] = [];
+var tile_pattern_entropies: Array[float] = [];
 
-var stack = null;
-var stack_size = 0;
+#This is the final output of the algorithm
+var collapsed_tiles: Array[int] = [];
+var possible_pattern_weights: Array[float] = [];
 
-var dx = [-1,0,1,0];
-var dy = [0,1,0,-1];
-var opposite = [2,3,0,1];
+var stack: Array = []; #Actually array of [int, int] (cant check, blame GDScript) (tile index, pattern index)
+var stack_size: int = 0;
+
+const DX: Array[int] = [-1,0,1,0];
+const DY: Array[int] = [0,1,0,-1];
+const OPPOSITE: Array[int] = [2,3,0,1];
 
 func pre_initialize(iterations: int, rng : RandomNumberGenerator) -> void:
-	fmx = model.final_width;
-	fmy = model.final_height;
-	fmx_x_fmy = fmx * fmy;
-	t = model.num_patterns;
-	
+	image_width_tiles = model.final_width;
+	image_height_tiles = model.final_height;
+	total_tile_count = image_width_tiles * image_height_tiles;
+	total_pattern_count = model.num_patterns;
+
 	weights = model.weights;
 	iterate(iterations, rng);
 	drawTiles();
 	#draw();
 
 func initialize():
-	distribution = []; # of t length
-	distribution.resize(model.num_patterns);
+	possible_pattern_weights = []; # of t length
+	possible_pattern_weights.resize(model.num_patterns);
 	wave = []; # of fmx_x_fmy len
-	compatible = []; # of fmx_x_fmy len
-	
-	for i in range(fmx_x_fmy):
-		wave.append([])
-		compatible.append([])
-		for _t in t:
+	compatibility_matrix = []; # of fmx_x_fmy len
+
+	for i in range(total_tile_count):
+		wave.append([]);
+		compatibility_matrix.append([]);
+		for _t in total_pattern_count:
 			wave[i].append([]);
-			wave[i].resize(t);
-			compatible[i].append([0,0,0,0])
-	
-	weight_log_weights = []
-	for _t in range(t):
-		weight_log_weights.append(0)
-	sums_of_weights = 0;
-	sum_of_weight_log_weights = 0;
-	
-	for _t in range(t):
-		weight_log_weights[_t] = (weights[_t] * log(weights[_t]))
-		sum_of_weights += weights[_t];
-		sum_of_weight_log_weights += weight_log_weights[_t];
-		
-	starting_entropy = log(sum_of_weights) - sum_of_weight_log_weights / sum_of_weights
-	
-	sums_of_ones = []
-	sums_of_weights = []
-	sums_of_weights_log_weights = []
-	entropies = []
-	for a in range(fmx_x_fmy):
-		sums_of_ones.append(0)
-		sums_of_weights.append(0)
-		sums_of_weights_log_weights.append(0)
-		entropies.append(0)
-	
-	stack = []
+			wave[i].resize(total_pattern_count);
+			compatibility_matrix[i].append([0,0,0,0]);
+
+	log_weighted_sums = [];
+	for _t in range(total_pattern_count):
+		log_weighted_sums.append(0);
+	tile_total_pattern_weights = [];
+	total_log_weighted_sums = 0;
+
+	for _t in range(total_pattern_count):
+		log_weighted_sums[_t] = (weights[_t] * log(weights[_t]));
+		total_weights += weights[_t];
+		total_log_weighted_sums += log_weighted_sums[_t];
+
+	initial_entropy = log(total_weights) - total_log_weighted_sums / total_weights;
+
+	tile_possible_pattern_count = [];
+	tile_total_pattern_weights = [];
+	tile_pattern_log_weight_sums = [];
+	tile_pattern_entropies = [];
+
+	for a in range(total_tile_count):
+		tile_possible_pattern_count.append(weights.size());
+		tile_total_pattern_weights.append(total_weights);
+		tile_pattern_log_weight_sums.append(total_log_weighted_sums);
+		tile_pattern_entropies.append(initial_entropy);
+
+	stack = [];
 	stack_size = 0;
 
 func observe(rng : RandomNumberGenerator):
 	var min_noise = 1000;
 	var argmin = -1;
-	for i in range(fmx_x_fmy):
-		if model.on_boundary(i % fmx, i / fmx | 0):
+	for i in range(total_tile_count):
+		if model.on_boundary(i % image_width_tiles, i / image_width_tiles | 0):
 			continue;
-		var amount = sums_of_ones[i];
+		var amount = tile_possible_pattern_count[i];
 		if amount == 0:
 			return false;
-		var entropy = entropies[i]
+		var entropy = tile_pattern_entropies[i]
 		if amount>1 && entropy <= min_noise:
 			var noise = 0.000001 * rng.randf_range(0,1); # DOUBLE CHECK
 			if entropy + noise < min_noise:
@@ -113,158 +116,156 @@ func observe(rng : RandomNumberGenerator):
 				argmin = i;
 	# search for the minimum entropy.
 	if argmin == -1:
-		observed = []
-		observed.resize(fmx_x_fmy);
-		for i in range(fmx_x_fmy):
-			for _t in range(t):
+		collapsed_tiles = [];
+		collapsed_tiles.resize(total_tile_count);
+		for i in range(total_tile_count):
+			for _t in range(total_pattern_count):
 				if wave[i][_t]:
-					observed[i] = _t;
+					collapsed_tiles[i] = _t;
 		return true;
-	
-	for _t in range(t):
-		distribution[_t] = weights[_t] if wave[argmin][_t] else 0;
-	var r = randomIndice(distribution, rng);
-	#print_debug(r)
+
+	for _t in range(total_pattern_count):
+		possible_pattern_weights[_t] = weights[_t] if wave[argmin][_t] else 0;
+	var r = randomIndice(possible_pattern_weights, rng);
 	var w = wave[argmin];
-	for _t in range(t):
+	for _t in range(total_pattern_count):
 		if w[_t] != (_t==r):
-			ban(argmin, _t)
+			ban(argmin, _t);
 	return null;
 
 func propagate():
 	while stack_size > 0:
-		var e1 = stack.pop_front();
+		var tile_element = stack.pop_front();
 		stack_size-=1;
-		
-		var i1 = e1[0]
-		var x1 = i1 % fmx
-		var y1 = i1 / fmx | 0
-		
-		for  d in range(4):
-			var _dx = dx[d]
-			var _dy = dy[d]
-			
-			var x2 = x1 + _dx
-			var y2 = y1 +_dy
-			
-			if model.on_boundary(x2,y2):
-				continue
-			if x2 < 0:
-				x2+=fmx
-			elif x2 >= fmx:
-				x2 -=fmx
-			if y2 < 0:
-				y2+=fmy
-			elif y2 >= fmy:
-				y2 -=fmy
-				
-			var i2 = x2 + y2 * fmx
-			var p = model.propagator[d][e1[1]]
-			var compat = compatible[i2]
-			
-			for l in range(p.size()):
-				var t2 = p[l]
-				var comp = compat[t2]
-				comp[d]-=1
-				if comp[d]==0:
-					ban(i2,t2)
-	
+
+		var tile_index = tile_element[0];
+		var tile_x_pos = tile_index % image_width_tiles;
+		var tile_y_pos = tile_index / image_width_tiles | 0;
+
+		for direction in range(4):
+			var delta_x = DX[direction];
+			var delta_y = DY[direction];
+
+			var neighbor_tile_x_pos = tile_x_pos + delta_x;
+			var neighbor_tile_y_pos = tile_y_pos + delta_y;
+
+			if model.on_boundary(neighbor_tile_x_pos, neighbor_tile_y_pos):
+				continue;
+			if neighbor_tile_x_pos < 0:
+				neighbor_tile_x_pos += image_width_tiles;
+			elif neighbor_tile_x_pos >= image_width_tiles:
+				neighbor_tile_x_pos -= image_width_tiles;
+			if neighbor_tile_y_pos < 0:
+				neighbor_tile_y_pos += image_height_tiles;
+			elif neighbor_tile_y_pos >= image_height_tiles:
+				neighbor_tile_y_pos -= image_height_tiles;
+
+			var neighbor_tile_index = neighbor_tile_x_pos + neighbor_tile_y_pos * image_width_tiles;
+			var possibleTiles = model.propagator[direction][tile_element[1]];
+			var compat = compatibility_matrix[neighbor_tile_index];
+
+			for l in range(possibleTiles.size()):
+				var patternIndex = possibleTiles[l];
+				var comp = compat[patternIndex];
+				comp[direction] -= 1;
+				if comp[direction] == 0:
+					ban(neighbor_tile_index, patternIndex);
+
 func singleIteration(rng : RandomNumberGenerator):
-	var result = observe(rng)
+	var result = observe(rng);
 	if result != null:
 		generation_complete = result;
 		return result;
 	propagate();
 	return null;
-	
+
 func iterate(iterations : int, rng) -> bool:
 	if wave == null:
-		initialize()
+		initialize();
 	if !initialized_field:
-		clear()
-	iterations = iterations || 0
+		clear();
+	iterations = iterations || 0;
 	if rng == null:
-		rng = RandomNumberGenerator.new()
+		rng = RandomNumberGenerator.new();
 	var i = 0;
 	while i < iterations || iterations == 0:
-		var result = singleIteration(rng)
+		var result = singleIteration(rng);
 		if result != null:
-			print_debug(result);
-			return result
-	print_debug("Finished.");
-	return true
-	
+			return result;
+	return true;
+
 func generate():
-	var rng = RandomNumberGenerator.new()
+	var rng = RandomNumberGenerator.new();
 	if wave == null:
-		initialize()
-	clear()
+		initialize();
+	clear();
 	while true:
-		var result = singleIteration(rng)
+		var result = singleIteration(rng);
 		if result != null:
-			return result
+			return result;
+	return false;
 
 func ban(i,_t):
-	var comp = compatible[i][_t];
+	var comp = compatibility_matrix[i][_t];
 	for d in range(4):
 		comp[d] = 0;
-	wave[i][_t] = false
-	
+	wave[i][_t] = false;
+
 	stack.push_back([i,_t]);
-	stack_size+=1
-	
-	sums_of_ones[i] -= 1;
-	sums_of_weights[i] -= weights[_t];
-	sums_of_weights_log_weights[i] -= weight_log_weights[_t];
-	
-	var sum = sums_of_weights[i]
-	entropies[i] = log(sum) - sums_of_weights_log_weights[i] / sum
+	stack_size+=1;
+
+	tile_possible_pattern_count[i] -= 1;
+	tile_total_pattern_weights[i] -= weights[_t];
+	tile_pattern_log_weight_sums[i] -= log_weighted_sums[_t];
+
+	var sum = tile_total_pattern_weights[i];
+	tile_pattern_entropies[i] = log(sum) - tile_pattern_log_weight_sums[i] / sum;
 
 func clear():
-	for i in range(fmx_x_fmy):
-		for _t in range(t):
-			wave[i][_t] = true
+	for i in range(total_tile_count):
+		for _t in range(total_pattern_count):
+			wave[i][_t] = true;
 			for d in range(4):
-				compatible[i][_t][d] = model.propagator[opposite[d]][_t].size();
-		sums_of_ones[i] = weights.size();
-		sums_of_weights[i] = sum_of_weights;
-		sums_of_weights_log_weights[i] = sum_of_weight_log_weights;
-		entropies[i] = starting_entropy
-	
-	initialized_field = true
-	generation_complete = false
-	
+				compatibility_matrix[i][_t][d] = model.propagator[OPPOSITE[d]][_t].size();
+		tile_possible_pattern_count[i] = weights.size();
+		tile_total_pattern_weights[i] = total_weights;
+		tile_pattern_log_weight_sums[i] = total_log_weighted_sums;
+		tile_pattern_entropies[i] = initial_entropy;
+
+	initialized_field = true;
+	generation_complete = false;
+
 func randomIndice(distrib : Array, rng : RandomNumberGenerator) -> int:
-	var r : float = rng.randf_range(0, 1);
-	var sum : float = 0;
-	var x : float = 0;
-	var i : int = 0;
-	
+	var random_value : float = rng.randf_range(0, 1);
+	var total_weight : float = 0;
+	var accumulated_weight : float = 0;
+	var index : int = 0;
+
 	for num in distrib:
-		sum += num;
+		total_weight += num;
 
-	r *= sum;
+	random_value *= total_weight;
 
-	while (r != 0 and i < distrib.size()):
-		x += distrib[i];
-		if (r <= x):
-			return i;
-		i+=1;
+	while (random_value != 0 and index < distrib.size()):
+		accumulated_weight += distrib[index];
+		if (random_value <= accumulated_weight):
+			return index;
+		index+=1;
 	return 0;
-	
-func draw():
+
+func drawTileIDs():
 	for i in range(16):
 		for j in range(16):
 			var text = Label.new();
 			text.position = Vector2(i * 16, j * 16);
-			text.text = str(observed[i + j * 16]);
+			text.text = str(collapsed_tiles[i + j * 16]);
 			get_tree().get_root().call_deferred("add_child", text);
-	
+
 
 func drawTiles():
 	var tilemap = Global.test_tilemap;
-	
-	var cardinalityTransformations = {
+
+	var cardinality_transformations: Dictionary = {
 		1: TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_V,
 		2: TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V,
 		3: TileSetAtlasSource.TRANSFORM_TRANSPOSE | TileSetAtlasSource.TRANSFORM_FLIP_H,
@@ -273,14 +274,13 @@ func drawTiles():
 		6: TileSetAtlasSource.TRANSFORM_FLIP_V,
 		7: TileSetAtlasSource.TRANSFORM_TRANSPOSE
 	}
-	
-	var currentIndex = 0;
-	
-	for tileY in range(model.final_width):
-		for tileX in range (model.final_height):
-			var tileData = model.tiles[observed[currentIndex]];
-			var transform = cardinalityTransformations.get(tileData[1], 0);
-			
-			tilemap.set_cell(Vector2i(tileX, tileY), 0, tileData[0], transform)
-			currentIndex += 1;
-			
+
+	var index = 0;
+
+	for tile_y in range(model.final_width):
+		for tile_x in range (model.final_height):
+			var tile_data = model.tiles[collapsed_tiles[index]];
+			var transform = cardinality_transformations.get(tile_data[1], 0);
+
+			tilemap.set_cell(Vector2i(tile_x, tile_y), 0, tile_data[0], transform);
+			index += 1;
